ebd88c5a36d03a961f66e8caa1926859b4870afe
knutinator <jonatanknut@gmail.com>
2023-02-13 10:03:33 +0000

    Sketch out game functions with comments

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# sketch of Battleboats program
+
+
+# a function that draws the board for the user and the computer
+# a function that randomly places boats for each player
+# a turn counter function that tells how long the game has been played
+# a while loop that keeps the game running until win or lose condition are met
+# a function that shows which player won the game and asks for a rematch
+
+# the main game loop, which includes:
+ 
+    
+        #player turn
+        # prompt the player for input 
+        # check if the player's input is valid (and returns error message)
+        # check whether the players input is a hit or miss
+        # draw the players input (and result) on the computer's game board
+        # check if any player has won the game
+
+        #cpu turn
+        # make a random selection on the board
+        # check if the cpu:s choice is a hit or miss
+        # draw the cpu:s choice on the players game board
+        # check if any player has won the game
+        # increase the turn counter by one
+
+
+
