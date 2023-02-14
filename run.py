8241f7069775e9bdd73043c44d4d9050a7baeeff
knutinator <jonatanknut@gmail.com>
2023-02-14 16:35:50 +0000

    Add method to create hidden cpu boats

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 # Inspiration taken from:
 # https://codereview.stackexchange.com/questions/232013/a-simple-battleship-game
 
@@ -8,7 +9,6 @@ print("\nB A T T L E B O A T S\n")
 
 # a function that draws the board for the user and the computer
 
-
 class Gameboard:
     def __init__(self, name, size,):
         self.name = name
@@ -16,16 +16,23 @@ class Gameboard:
         self.game_board = None
         self.x = None
         self.y = None
+        self.cpu_board_inv = None
         
-
+        
     def make_board(self):
         """
         Draws a gameboard based on size argument.
         Places a number of boats randomly, boat count is based on size arg.
         """
         print(self.name)
+        # makes both boards
         self.game_board = [["."] * self.size for _ in range(self.size)]
         
+        # makes an invisible board for cpu
+        if self.name == "COMPUTER":
+            self.cpu_board_inv = [["o"] * self.size for _ in range(self.size)]
+        
+        # generate random number
         count = 0
         while count < self.size:
             self.x = randint(0, self.size-1)
@@ -36,15 +43,16 @@ class Gameboard:
             if self.name == "PLAYER":
                 if self.game_board[self.x][self.y] == ".":
                     self.game_board[self.x][self.y] = "B"
-        
+            if self.name == "COMPUTER":
+                if self.cpu_board_inv[self.x][self.y] == "o":
+                    self.cpu_board_inv[self.x][self.y] = "B"    
+                
         for point in self.game_board:
             print(*point)
         print("\n")
-
-        # makes an invisible board
+        
         if self.name == "COMPUTER":
-            cpu_board_inv = [[self.size] * self.size] * self.size
-            print(cpu_board_inv) 
+            print(f"invisible: {self.cpu_board_inv}")
 
         
     def check_valid(self, x_row, y_column):
@@ -52,7 +60,7 @@ class Gameboard:
 
 
     def check_hit(self, x_row, y_column):
-        print("check hit")    
+        print("check_hit")    
 
 
     def redraw_board(self):  
@@ -73,16 +81,19 @@ class Gameboard:
 # a while loop that keeps the game running until win or lose condition are met
 # a function that shows which player won the game and asks for a rematch
 
-# the main game loop, which includes:
+# the main game loop
  
+# NEEDS TO BE A WHILE LOOP, THAT RUNS UNTIL SOMEONE WINS
+
 
 def run_game():
+    # These must be outside loop: (put into populate_game function)
     player_board = Gameboard("PLAYER", 5)
     player_board.make_board()
 
     cpu_board = Gameboard("COMPUTER", 5)
     cpu_board.make_board()   
-
+    
 #player turn
     # prompt the player for input  
     print("Guess where the computer's boats are!\n")
