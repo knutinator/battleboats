670905072200b2645c7174be45cacfc72dc93ee6
knutinator <jonatanknut@gmail.com>
2023-02-14 09:17:29 +0000

    move generate coordinate function into class

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 from random import randint
 # sketch of Battleboats program
 
+# Inspiration taken from:
+# https://codereview.stackexchange.com/questions/232013/a-simple-battleship-game
+
 # function to start a new game
 
 # a function that draws the board for the user and the computer
 class Gameboard:
-    def __init__(self, name, size):
+    def __init__(self, name, size,):
         self.name = name
         self.size = size
+        
 
     def make_board(self):
         print(self.name)
@@ -16,9 +20,15 @@ class Gameboard:
             print(*point)
         print("\n")
 
+    def make_boats(self):
+        x = randint(0, self.size)
+        y = randint(0, self.size)
+        print(x, y)
+
 
 player_board = Gameboard("PLAYER", 5)
 player_board.make_board()
+player_board.make_boats()
 
 cpu_board = Gameboard("CPU", 5)
 cpu_board.make_board()
@@ -27,13 +37,15 @@ cpu_board.make_board()
 # a function that randomly places boats for each player
     # select a random column and a random row in the board
 
-
-def make_boats():
-    x = player_board.size 
-    return randint(0, x), randint(0, x)
     # generate a boat
+    # item x in list y in player_board list
+    
+
+
+
+
     # loop 5 times
-print(make_boats())
+
 
 # a turn counter function that tells how long the game has been played
 # a while loop that keeps the game running until win or lose condition are met
