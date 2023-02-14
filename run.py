6a289e24ffc0aa2f229e27d2ea6543bc8b9d7131
knutinator <jonatanknut@gmail.com>
2023-02-14 07:44:55 +0000

    Add function to draw a game board

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 # sketch of Battleboats program
 
+# function to start a new game
 
 # a function that draws the board for the user and the computer
+
+def makeboard(board_size):
+    game_board = [["."] * board_size for _ in range(board_size)]
+    for point in game_board:
+        print(*point)
+
+makeboard(5)
+
 # a function that randomly places boats for each player
 # a turn counter function that tells how long the game has been played
 # a while loop that keeps the game running until win or lose condition are met
@@ -10,19 +19,16 @@
 # the main game loop, which includes:
  
     
-        #player turn
-        # prompt the player for input 
-        # check if the player's input is valid (and returns error message)
-        # check whether the players input is a hit or miss
-        # draw the players input (and result) on the computer's game board
-        # check if any player has won the game
+#player turn
+# prompt the player for input         
+# check if the player's input is valid (and returns error message)
+# check whether the players input is a hit or miss
+# draw the players input (and result) on the computer's game board
+# check if any player has won the game
 
-        #cpu turn
-        # make a random selection on the board
-        # check if the cpu:s choice is a hit or miss
-        # draw the cpu:s choice on the players game board
-        # check if any player has won the game
-        # increase the turn counter by one
-
-
-
+#cpu turn
+# make a random selection on the board
+# check if the cpu:s choice is a hit or miss
+# draw the cpu:s choice on the players game board
+# check if any player has won the game
+# increase the turn counter by one
