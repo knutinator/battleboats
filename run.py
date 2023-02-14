0d8622f374902a51a319b68af4f5b95b2a081b92
knutinator <jonatanknut@gmail.com>
2023-02-14 10:58:33 +0000

    Add function that asks for player input

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from random import randint
 # https://codereview.stackexchange.com/questions/232013/a-simple-battleship-game
 
 # function to start a new game
+print("\nB A T T L E B O A T S\n")
 
 # a function that draws the board for the user and the computer
 class Gameboard:
@@ -22,11 +23,11 @@ class Gameboard:
         game_board = [["."] * self.size for _ in range(self.size)]
         count = 0
         while count < self.size:
-            x = randint(0, self.size)
-            y = randint(0, self.size)
+            x = randint(0, self.size - 1)
+            y = randint(0, self.size - 1)
             count += 1
-            if game_board[x-1][y-1] == ".":
-                game_board[x-1][y-1] = "B"
+            if game_board[x][y] == ".":
+                game_board[x][y] = "B"
 
         for point in game_board:
             print(*point)
@@ -36,21 +37,17 @@ class Gameboard:
 player_board = Gameboard("PLAYER", 5)
 player_board.make_board()
 
-cpu_board = Gameboard("CPU", 5)
+cpu_board = Gameboard("COMPUTER", 5)
 cpu_board.make_board()
 
+
+# Function that hides the CPU boats, but keeps them in memory
 
 
 # a function that randomly places boats for each player
     # select a random column and a random row in the board
-
     # generate a boat
     # item x in list y in player_board list
-    
-
-
-
-
     # loop 5 times
 
 
@@ -60,17 +57,26 @@ cpu_board.make_board()
 
 # the main game loop, which includes:
  
-    
+def run_game():    
 #player turn
-# prompt the player for input         
-# check if the player's input is valid (and returns error message)
-# check whether the players input is a hit or miss
-# draw the players input (and result) on the computer's game board
-# check if any player has won the game
+    # prompt the player for input  
+    print("Guess where the computer's boats are!")
+    row = int(input(f"Enter a row number between 1 - {player_board.size}: "))
+    column = int(input(f"Enter a column number between 1 - {player_board.size}: "))
+    # a function that decreases the players input by 1, to get correct index
+    x_row = row - 1
+    y_column = column - 1
+
+    # check if the player's input is valid (and returns error message)
+    # check whether the players input is a hit or miss
+    # draw the players input (and result) on the computer's game board
+    # check if any player has won the game
 
 #cpu turn
-# make a random selection on the board
-# check if the cpu:s choice is a hit or miss
-# draw the cpu:s choice on the players game board
-# check if any player has won the game
-# increase the turn counter by one
+    # make a random selection on the board
+    # check if the cpu:s choice is a hit or miss
+    # draw the cpu:s choice on the players game board
+    # check if any player has won the game
+    # increase the turn counter by one
+
+run_game()
