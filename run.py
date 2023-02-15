e3c442be95cd47661549d63ad208021fce20b91d
knutinator <jonatanknut@gmail.com>
2023-02-15 12:10:52 +0000

    Add logic to determine winner and end game

--- run.py
+++ run.py
@@ -5,10 +5,6 @@ from random import randint
 
 # function to start a new game
 
-game_over = 0
-
-
-
 class Gameboard:
     def __init__(self, name, size,):
         self.name = name
@@ -20,6 +16,8 @@ class Gameboard:
         self.cpu_board_inv = None
         self.player_score = 0
         self.cpu_score = 0
+        self.game_over = 0
+    
         
     # method to draw the board for the user and the computer    
     def make_board(self):
@@ -78,41 +76,47 @@ class Gameboard:
         print("check_valid")    
 
 
-
     # redraw board
     def redraw_board(self, x_row, y_column, cpu_x_row, cpu_y_column):  
         print(f"{self.name} BOARD")
         if self.name == "COMPUTER": # Computer board (bottom)
             if not self.cpu_board_inv[x_row][y_column] == "B":
-                
                 self.cpu_game_board[x_row][y_column] = "o"
                 for point in self.cpu_game_board:
                     print(*point)
-                print("Splash! You missed...")    
+                print("SPLASH! You missed...")    
                 print(f"Your score: {self.player_score}")  
             else:
                 self.cpu_game_board[x_row][y_column] = "#"                
                 for point in self.cpu_game_board:
                     print(*point)
                 self.player_score += 1  
-                print("Boom! You hit a boat!")
-                print(f"Your score: {self.player_score}")  
-                    
+                print("BOOM! You hit a boat!")
+                print(f"Your score: {self.player_score}")
+                            
         elif self.name == "PLAYER": # Player board (top)
             if not self.pl_game_board[cpu_x_row][cpu_y_column] == "B":
                 self.pl_game_board[cpu_x_row][cpu_y_column] = "o"
                 for point in self.pl_game_board:
                     print(*point)
-                print("Splash! Computer missed!")
+                print("SPLASH! Computer missed!")
                 print(f"CPU score: {self.cpu_score}")      
             else:
                 self.pl_game_board[cpu_x_row][cpu_y_column] = "#"
                 for point in self.pl_game_board:
                     print(*point)
-                print("Boom! Computer sank your boat...")
-                self.player_score += 1
+                print("BOOM! Computer sank your boat...")
+                self.cpu_score += 1
                 print(f"CPU score: {self.cpu_score}")    
-         
+        
+        if self.cpu_score == self.size:
+            print("cpu wins")
+            self.game_over = 1
+
+        if self.player_score == self.size:
+            print("player wins")
+            self.game_over = 1    
+
         print("\n")  
 
     
@@ -131,25 +135,21 @@ def new_game():
     player_board.make_board()
     cpu_board = Gameboard("COMPUTER", 5)
     cpu_board.make_board()  
+    game_over = 0
 
-    
-        
     # main game loop:
-    while game_over == 0:
+    while player_board.game_over or cpu_board.game_over == 0:
         # player turn
         # prompt the player for input  
         print("Guess where the computer's boats are!\n")
-        row = int(input(f"Enter a row number between 1 - {player_board.size}: "))
-        column = int(input(f"Enter a column number between 1 - {player_board.size}: "))
+        row = int(input(f"Enter a row nr between 1 - {player_board.size}: "))
+        column = int(input(f"Enter a column nr between 1 - {player_board.size}: "))
         # decreases the players input by 1, to get correct index (0-4)
         x_row = row - 1
         y_column = column - 1
 
         # check if the player's input is valid (and returns error message)
         player_board.check_valid(x_row, y_column)
-
-        
-    
 
         # draw the players input (and result) on the computer's game board
         # check if any player has won the game
@@ -175,14 +175,8 @@ def new_game():
         player_board.redraw_board(x_row, y_column, cpu_x_row, cpu_y_column)
         cpu_board.redraw_board(x_row, y_column, cpu_x_row, cpu_y_column)
 
-
-        if game_over == 1:
-            declare_winner()
+        if player_board.game_over or cpu_board.game_over == 1:
             break
-    
+        
 
 new_game()
-
-
-def declare_winner():
-    print("winner")
